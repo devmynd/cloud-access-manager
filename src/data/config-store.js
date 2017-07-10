3a7f3d13e52f21ef3ab77a1e72aee1ab7cc38b91
Michael Evans <michael.evans@devmynd.com>
2017-07-10 09:29:19 -0500

    fix config test when there is not already a file

--- src/data/config-store.js
+++ src/data/config-store.js
@@ -1,14 +1,21 @@
 // @flow
-var fs = require('file-system')
+import * as fs from 'file-system'
+// var fs = require('file-system')
 
 const filePath = './.services.json'
 
 export class ConfigStore {
   save (moduleName: string, config: any): void {
-    let json = fs.readFileSync(filePath, 'utf8')
-    var data = JSON.parse(json)
-    data[moduleName] = config
+    var data
 
+    if (fs.existsSync(filePath)) {
+      let json = fs.readFileSync(filePath, 'utf8')
+      data = JSON.parse(json)
+    } else {
+      data = {}
+    }
+
+    data[moduleName] = config
     fs.writeFileSync(filePath, JSON.stringify(data))
   }
 
