a6bf4b3d1e3715e5c5002b12849b51891d668814
Michael Evans <michael.evans@devmynd.com>
2017-07-11 10:09:07 -0500

    add missing flow annotation

--- src/types/index.js
+++ src/types/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 export type { UserAccount } from './user-account'
 export type { ServiceProvider, ServiceProviderModule } from './service-provider'
 export type { UserServiceSummary } from './user-service-summary'
