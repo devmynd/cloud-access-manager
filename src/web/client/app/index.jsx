f02243944e2c9aaaeda5f6733e7011ce2ff59102
Shamyle <shamyleg@gmail.com>
2017-07-19 16:55:49 -0500

    add back flow and standard

--- src/web/client/app/index.jsx
+++ src/web/client/app/index.jsx
@@ -1,7 +1,7 @@
 // @flow
 
-import React from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import {render} from 'react-dom'
 
 function sayHello (to: string): string {
   return `hello ${to}`
@@ -9,8 +9,8 @@ function sayHello (to: string): string {
 
 class App extends React.Component {
   render () {
-    return <p>{sayHello("Shamyle")}</p>;
+    return <p>{sayHello('Shamyle')}</p>
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
-var webpack = require('webpack');
-var path = require('path');
+var path = require('path')
 
-var BUILD_DIR = path.resolve(__dirname, 'src/web/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/web/client/app');
+var BUILD_DIR = path.resolve(__dirname, 'src/web/client/public')
+var APP_DIR = path.resolve(__dirname, 'src/web/client/app')
 
 var config = {
   entry: APP_DIR + '/index.jsx',
@@ -10,15 +9,15 @@ var config = {
     path: BUILD_DIR,
     filename: 'bundle.js'
   },
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel-loader'
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'babel-loader'
       }
     ]
   }
-};
+}
 
-module.exports = config;
+module.exports = config
