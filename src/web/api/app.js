4cb7e9997de9fdf624ff010e764bb488de06f60e
Shamyle <shamyleg@gmail.com>
2017-07-19 16:49:54 -0500

    setup react client website

--- src/web/api/app.js
+++ src/web/api/app.js
@@ -4,7 +4,9 @@
 import express from 'express'
 const app = express()
 
-app.use(express.static('src/web/public'))
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
---
+++ src/web/client/app/index.jsx
@@ -0,0 +1,16 @@
+// @flow
+
+import React from 'react';
+import {render} from 'react-dom';
+
+function sayHello (to: string): string {
+  return `hello ${to}`
+}
+
+class App extends React.Component {
+  render () {
+    return <p>{sayHello("Shamyle")}</p>;
+  }
+}
+
+render(<App/>, document.getElementById('app'));
