ba3cba8bf80ba4f593dfc8a88784a45f6c5a35a1
Shamyle <shamyleg@gmail.com>
2017-07-26 16:30:17 -0500

    Create navbar and link components

--- src/web/client/src/index.js
+++ src/web/client/src/index.js
@@ -4,14 +4,44 @@ import React from 'react'
 import {render} from 'react-dom'
 import '../styles/index.scss'
 import 'bulma/bulma.sass'
+import { Switch, Link, Route, BrowserRouter } from 'react-router-dom'
 
-function sayHello (to: string): string {
-  return `hello ${to}`
+const NavBar = (props) => {
+  return (
+    <nav className="navbar ">
+      <div className="navbar-menu">
+        <div className="navbar-start">
+          {props.children}
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+const NavBarLink = (props) => {
+  const isActive = props.to == window.location.pathname
+  return <Link className={`navbar-item ${isActive ? 'is-active' : ''}`} to={props.to}>{props.children}</Link>
 }
 
 class App extends React.Component {
   render () {
-    return <p className='hello-world title'>{sayHello('Shamyle')}</p>
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <NavBar>
+            <NavBarLink to='/'>Home</NavBarLink>
+            <NavBarLink to='/audit'>Audit</NavBarLink>
+            <NavBarLink to='/groups'>Groups</NavBarLink>
+            <NavBarLink to='/individuals'>Individuals</NavBarLink>
+            <NavBarLink to='/services'>Services</NavBarLink>
+          </NavBar>
+          <Switch>
+            <Route exact path='/' component={() => <h1>Welcome to CAM</h1>} />
+            <Route component={() => <p>not found</p>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
   }
 }
 
