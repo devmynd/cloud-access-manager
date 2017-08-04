dbfde490a0c5408af1154417b0cbea669b66dd37
Michael Evans <michael.evans@devmynd.com>
2017-08-04 09:33:20 -0500

    setup audit command algorithm

--- src/cli/commands/audit-command.js
+++ src/cli/commands/audit-command.js
@@ -25,67 +25,54 @@ export async function audit () {
 }
 
 export async function interactiveAudit () {
-  // const accounts = await manager.download('all')
-  // const auditor = new Auditor(individualStore, groupStore)
-  //
-  // let flaggedAccounts = auditor.performAudit(accounts)
-  //
-  // const newIndividualEmails = flaggedAccounts
-  //   .filter((account) => account.isNewIndividual)
-  //   .map((account) => account.email)
-  //
-  // if (newIndividualEmails.length > 0) {
-  //   const groupNames = groupStore.getAll().map((group) => group.name)
-  //   for (let i = 0; i < newIndividualEmails.length; i++) {
-  //     const email = newIndividualEmails[i]
-  //     const selectedGroups = await selectGroupsForEmail(email, groupNames)
-  //     let user = individualStore.getByEmail(email)
-  //     user.groups = selectedGroups
-  //     individualStore.save(user)
-  //   }
-  //
-  //   // Perform another audit to refresh after having selected group membership
-  //   flaggedAccounts = auditor.performAudit(accounts)
-  // }
-  //
-  // for (let i = 0; i < flaggedAccounts.length; i++) {
-  //   const account = flaggedAccounts[i]
-  //   const individual = individualStore.getByEmail(account.email)
-  //   await auditForIndividual(account, individual)
-  //   term('\n')
-  // }
-  //
-  // flaggedAccounts = auditor.performAudit(accounts)
-  // printFlaggedAccounts(flaggedAccounts)
-  console.log('todo: refactor audit command')
+  const accounts: Array<ServiceUserAccount> = await manager.download('all')
+  const auditor = new Auditor(individualStore, groupStore)
+
+  for (let accountIndex = 0; accountIndex < accounts.length; accountIndex++) {
+    const account = accounts[accountIndex]
+
+    const flag = auditor.auditAccount(account)
+    if (flag) {
+      if (flag.individual) {
+        auditForIndividual(flag.individual, flag.serviceId, flag.assets)
+      } else {
+        // prompt to ask about creating individual or adding to existing individual
+        // then call:
+        // auditForIndividual(createdOrUpdatedIndividual, flag.serviceId, flag.assets)
+      }
+    }
+  }
+
+  // re-run the audit with the non-interactive version.
+  audit()
 }
 
-// async function auditForIndividual (account: ServiceUserAccountsAggregate, individual: Individual): Promise<void> {
-//   term.cyan.bold(`${account.email}\n`)
-//
-//   const whitelistedPartition = lodash.partition(account.assetAssignments, (assetAssignment) => individual.accessRules.hasOwnProperty(assetAssignment.service.id))
-//
-//   let auditableAssetAssignments = whitelistedPartition[0]
-//   const newServices = whitelistedPartition[1]
-//
-//   if (newServices.length > 0) {
-//     const selectedAssetAssignments = await selectServices(newServices)
-//     auditableAssetAssignments = auditableAssetAssignments.concat(selectedAssetAssignments)
-//   }
-//
-//   const newAccessRules = await auditServices(auditableAssetAssignments)
-//   Object.keys(newAccessRules).forEach((serviceId) => {
-//     const newServiceAccessRules = newAccessRules[serviceId]
-//     let individualServiceAccessRules = individual.accessRules[serviceId]
-//     if (individualServiceAccessRules) {
-//       individual.accessRules[serviceId] = individualServiceAccessRules.concat(newServiceAccessRules)
-//     } else {
-//       individual.accessRules[serviceId] = newServiceAccessRules
-//     }
-//   })
-//
-//   individualStore.save(individual)
-// }
+async function auditForIndividual (individual: Individual, serviceId: string, assets: Array<Asset>): Promise<void> {
+  // term.cyan.bold(`${account.email}\n`)
+  //
+  // const whitelistedPartition = lodash.partition(account.assetAssignments, (assetAssignment) => individual.accessRules.hasOwnProperty(assetAssignment.service.id))
+  //
+  // let auditableAssetAssignments = whitelistedPartition[0]
+  // const newServices = whitelistedPartition[1]
+  //
+  // if (newServices.length > 0) {
+  //   const selectedAssetAssignments = await selectServices(newServices)
+  //   auditableAssetAssignments = auditableAssetAssignments.concat(selectedAssetAssignments)
+  // }
+  //
+  // const newAccessRules = await auditServices(auditableAssetAssignments)
+  // Object.keys(newAccessRules).forEach((serviceId) => {
+  //   const newServiceAccessRules = newAccessRules[serviceId]
+  //   let individualServiceAccessRules = individual.accessRules[serviceId]
+  //   if (individualServiceAccessRules) {
+  //     individual.accessRules[serviceId] = individualServiceAccessRules.concat(newServiceAccessRules)
+  //   } else {
+  //     individual.accessRules[serviceId] = newServiceAccessRules
+  //   }
+  // })
+  //
+  // individualStore.save(individual)
+}
 //
 // async function auditServices (assetAssignments: Array<AssetAssignment>): Promise<ServiceAccessHash> {
 //   const serviceAccess: ServiceAccessHash = {}
