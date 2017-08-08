687f7f10e6e5e464bd9a4b37ee8b882f68207ed1
Michael Evans <michael.evans@devmynd.com>
2017-08-08 11:58:24 -0500

    WIP: github integration

--- src/core/service-providers/github.js
+++ src/core/service-providers/github.js
@@ -1,69 +1,66 @@
 // @flow
 import type { ServiceProvider, ServiceProviderModule } from '../types'
+import GitHub from 'github-api'
+import lodash from 'lodash'
 
-let configKeys = ['Organization Login']
+let configKeys = ['OAuth Token', 'Organization Name']
 
 class GitHubProvider implements ServiceProvider {
-  login: string
+  api: GitHub
+  orgName: string
 
   constructor (config: Object) {
-    this.login = config['Organization Login']
+    this.api = new GitHub({
+      token: config['OAuth Token']
+    })
+    this.orgName = config['Organization Name']
   }
 
-  listAccounts () {
-    return new Promise((resolve, reject) => {
-      resolve([
-        {
-          identity: {
-            userId: 'shamwow',
-            fullName: 'Shamyle Ghazali'
-          },
-          assets: [{
-            name: 'repo a',
-            role: 'read'
-          }, {
-            name: 'repo b',
-            role: 'write'
-          }]
-        },
-        {
-          identity: {
-            userId: 'evansmd01',
-            fullName: 'Michael'
-          },
-          assets: [{
-            name: 'repo a',
-            role: 'admin'
-          }, {
-            name: 'repo b',
-            role: 'write'
-          }]
-        },
-        {
-          identity: {
-            email: 'ty@devmynd.com'
-          },
-          assets: [{
-            name: 'repo a',
-            role: 'admin'
-          }, {
-            name: 'repo b',
-            role: 'write'
-          }]
+  async listAccounts () {
+    const org = this.api.getOrganization(this.orgName)
+    let repos = (await org.getRepos()).data
+
+    //repos = lodash.filter(repos, (r) => r.name === "cloud-access-manager")
+
+    const repoCollabs = await Promise.all(repos.map(async (repo) => {
+      let repoWrapper = this.api.getRepo(repo.owner.login, repo.name)
+      const collabs = await repoWrapper.getCollaborators()
+      return {
+        repo: repo,
+        collabs: collabs.data
+      }
+    }))
+
+    let accountsHash = {}
+
+    repoCollabs.forEach((repoCollab) => {
+      repoCollab.collabs.forEach((collaborator) => {
+
+        if(!accountsHash.hasOwnProperty(collaborator.login)){
+          accountsHash[collaborator.login] = { identity: { userId: collaborator.login }, assets: [] }
         }
-      ])
+        const account = accountsHash[collaborator.login]
+
+        account.assets.push({
+          name: repoCollab.repo.full_name,
+          role: collaborator.permissions.admin ? 'admin' : collaborator.permissions.push ? 'read/write' : 'read-only'
+        })
+      })
     })
+
+    return Object.keys(accountsHash).map((key) => accountsHash[key])
   }
 
   async testConnection () {
-    // todo: implement test connection
+    const org = this.api.getOrganization(this.orgName)
+    const repos = await org.getRepos()
   }
 }
 
 export const gitHubProviderModule: ServiceProviderModule = {
   id: 'github',
   displayName: 'GitHub',
-  roles: ['read', 'write', 'admin'],
+  roles: ['read-only', 'read/write', 'admin'],
   configKeys: configKeys,
   providerFactory (config) {
     return new GitHubProvider(config)
