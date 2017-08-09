d38db988b82e75a501d8031214fed3033491fa4e
Shamyle <shamyleg@gmail.com>
2017-08-09 10:18:52 -0500

    Fix mapIndividual to resolve flow issues

--- src/web/api/graphql/resolvers/accounts-resolvers.js
+++ src/web/api/graphql/resolvers/accounts-resolvers.js
@@ -4,7 +4,7 @@ import { manager } from '../../../../core/service-providers/manager'
 import { Auditor } from '../../../../core/auditor'
 import { individualStore } from '../../../../core/data/individual-store'
 import { groupStore } from '../../../../core/data/group-store'
-import type { FlaggedInfo } from '../../../../core/types'
+import type { FlaggedInfo, Individual } from '../../../../core/types'
 
 export function listAccounts (args: { serviceId: string }) {
   return manager.download(args.serviceId || 'all')
@@ -17,7 +17,7 @@ export async function performAudit () {
     const flag = auditor.auditAccount(account)
     if (flag) {
       result.push({
-        individual: mapIndividual(individual),
+        individual: mapIndividual(flag.individual),
         serviceId: flag.serviceId,
         userIdentity: flag.userIdentity,
         assets: flag.assets
@@ -28,18 +28,22 @@ export async function performAudit () {
 }
 
 function mapIndividual(individual: ?Individual) {
-  if (individual) {
+  if (individual && individual.serviceUserIdentities) {
     return {
       id: individual.id,
       fullName: individual.fullName,
       primaryEmail: individual.primaryEmail,
       serviceUserIdentities: Object.keys(individual.serviceUserIdentities).map((serviceId) => {
-        return { serviceId: serviceId, userIdentity: individual.serviceUserIdentities[serviceId] }
+        if (individual && individual.serviceUserIdentities) {
+          return { serviceId: serviceId, userIdentity: individual.serviceUserIdentities[serviceId] }
+        }
       }),
       accessRules: Object.keys(individual.accessRules).map((serviceId) => {
-        return {
-          service: manager.getServiceInfo(serviceId),
-          accessRules: individual.accessRules[serviceId]
+        if (individual && individual.accessRules) {
+          return {
+            service: manager.getServiceInfo(serviceId),
+            accessRules: individual.accessRules[serviceId]
+          }
         }
       }),
       groups: individual.groups
