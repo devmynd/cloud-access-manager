f8c2b0bbcc936e2f58c9fa9ef023b1c808cb1728
Shamyle <shamyleg@gmail.com>
2017-08-14 10:43:54 -0500

    List of existing individuals appears during autocompletion

--- src/web/client/src/components/shared/individual-search.js
+++ src/web/client/src/components/shared/individual-search.js
@@ -1,18 +1,45 @@
 import React from 'react'
 import TypeAheadInput from './type-ahead-input'
+import MessagesContainer from './messages-container'
+import graphqlApi from '../../graphql-api'
 
 export default class IndividualSearch extends React.Component {
 
-
-  query = (text, callback) => {
-    callback([
-      {
-        fullName: "bob"
-      },
-      {
-        fullName: "john"
+  query = async (text, callback) => {
+    const query = `{
+    	individuals(fuzzySearch:"${text}", limit: ${10}) {
+        id
+          primaryEmail
+        	fullName
+          serviceUserIdentities {
+            serviceId
+            userIdentity {
+              email
+              userId
+            }
+          }
+          accessRules {
+            service {
+              id
+            }
+            accessRules {
+              asset
+              role
+            }
+          }
+          groups
       }
-    ])
+    }`
+
+    const response = await graphqlApi.request(query)
+    if (response.error) {
+      this.messagesContainer.push({
+        title: "Could not access existing individuals",
+        body: response.error.message
+      })
+      return
+    }
+    callback(response.data.individuals)
   }
 
   renderIndividual = (individual) => {
@@ -23,12 +50,15 @@ export default class IndividualSearch extends React.Component {
 
   render() {
     return (
-      <TypeAheadInput
-        placeholder="Search for individual by name or email"
-        query={this.query}
-        matchRenderer={this.renderIndividual}
-        onMatchSelected={this.props.onIndividualSelected}
-      />
+      <div>
+        <TypeAheadInput
+          placeholder="Search for individual by name or email"
+          query={this.query}
+          matchRenderer={this.renderIndividual}
+          onMatchSelected={this.props.onIndividualSelected}
+        />
+        <MessagesContainer ref={(container) => { this.messagesContainer = container }} />
+      </div>
     )
   }
 }
