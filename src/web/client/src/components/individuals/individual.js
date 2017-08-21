067b28f607106f646e2842bfb475641cb1e85a03
Shamyle <shamyleg@gmail.com>
2017-08-21 16:04:29 -0500

    User can view the group access rules of an individual

--- src/web/client/src/components/individuals/individual.js
+++ src/web/client/src/components/individuals/individual.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import IndividualSearch from '../shared/individual-search'
+import graphqlApi from '../../graphql-api'
 
 export default class Individual extends React.Component {
   state = {
-    individual: null
+    individual: null,
+    groups: []
+  }
+
+  componentDidMount = async () => {
+    const query = `{
+     groups {
+      name
+      serviceAccessRules {
+        service {
+          id
+          displayName
+        }
+        accessRules {
+          asset
+          role
+        }
+      }
+     }
+    }`
+
+    const response = await graphqlApi.request(query)
+
+    if (!response.error) {
+      this.setState({
+        groups: response.data.groups
+      })
+    }
   }
 
   onIndividualSelected = (individual) => {
@@ -12,22 +40,30 @@ export default class Individual extends React.Component {
     })
   }
 
+  mapToAccessRuleDescriptions = (serviceAccessRules) => {
+    let accessRuleDescriptions = []
+    serviceAccessRules.forEach((serviceAccess) => {
+      serviceAccess.accessRules.forEach((rule) => {
+        let desc = serviceAccess.service.displayName
+        if (rule.asset !== "*") {
+          desc += ` / ${rule.asset}`
+        }
+        if (rule.role !== "*") {
+          desc += ` / ${rule.role}`
+        }
+        accessRuleDescriptions.push(desc)
+      })
+    })
+    return accessRuleDescriptions
+  }
+
   render() {
     const individual = this.state.individual
     let accessRuleDescriptions = []
+    let memberOfGroups = []
     if (individual) {
-      individual.accessRules.forEach((serviceAccess) => {
-        serviceAccess.accessRules.forEach((rule) => {
-          let desc = serviceAccess.service.displayName
-          if (rule.asset !== "*") {
-            desc += ` / ${rule.asset}`
-          }
-          if (rule.role !== "*") {
-            desc += ` / ${rule.role}`
-          }
-          accessRuleDescriptions.push(desc)
-        })
-      })
+      accessRuleDescriptions = this.mapToAccessRuleDescriptions(individual.accessRules)
+      memberOfGroups = this.state.groups.filter((group) => individual.groups.includes(group.name))
     }
 
     return (
@@ -55,16 +91,20 @@ export default class Individual extends React.Component {
               </div>
             </section>
 
-            <section className="section">
-              <div className="container">
-                <h2 className="subtitle">Groups</h2>
-                <ul>
-                  {
-                    individual.groups.map((g) => <li key={g}>{g}</li>)
-                  }
-                </ul>
-              </div>
-            </section>
+            {
+              memberOfGroups.map((group) => (
+                <section key={group.name} className="section">
+                  <div className="container">
+                    <h2 className="subtitle">Group '{group.name}' Access Rules</h2>
+                    <ul>
+                      {
+                        this.mapToAccessRuleDescriptions(group.serviceAccessRules).map((d) => <li key={d}>{d}</li>)
+                      }
+                    </ul>
+                  </div>
+                </section>
+              ))
+            }
           </div>
         }
       </div>
